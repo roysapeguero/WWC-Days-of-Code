caaac3b6b543c0821caa89ef7a6ff20b16f1fbd6
roysa <roysapeguero@outlook.com>
2024-01-14 23:37:23 -0800

    Complete python day5

---
+++ python/05-max-and-min.py
@@ -0,0 +1,7 @@
+# Today's Challenge:
+# Write a program to find the maximum and minimum values in a list.
+
+num_list = [-4, 1, 3, 6, 10, -2]
+max_num, min_num = max(num_list), min(num_list)
+print(f"Max value: {max_num}")
+print(f"Min value: {min_num}")
