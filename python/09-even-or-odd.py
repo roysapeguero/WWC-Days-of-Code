fe764d63fda5938a56cf0fb3624e7da200c59709
roysa <roysapeguero@outlook.com>
2024-01-18 23:49:20 -0800

    Complete python day9

---
+++ python/09-even-or-odd.py
@@ -0,0 +1,12 @@
+# Today's Challenge:
+# Write a program to check if a number is even or odd.
+
+def even_or_odd(n):
+  if n % 2 == 0:
+    return 'Even'
+  else:
+    return 'Odd'
+
+print(even_or_odd(5))
+print(even_or_odd(80))
+print(even_or_odd(1))
